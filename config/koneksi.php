d12e9451bcf6a137f4d857b87c34157bda2b8dcf
GrandMoze <164888727+GrandMoze@users.noreply.github.com>
2024-10-25 20:43:39 +0700

    Delete config directory

--- config/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $koneksi = new mysqli("localhost", "root", "", "ujian tengah semester lecture");
-
-   if ($koneksi->connect_error) {
-       die("Koneksi gagal: " . $koneksi->connect_error);
-   }
-   ?>
